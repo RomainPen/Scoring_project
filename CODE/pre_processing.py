6dfe18d968612a54ac9cbc369479783f1b471d3a
RomainPen <romain.pen.pro16@gmail.com>
2023-07-09 15:07:32 +0200

    update

--- CODE/pre_processing.py
+++ CODE/pre_processing.py
@@ -36,63 +36,65 @@ class pre_processing :
         Returns:
             pd.DataFrame: encoded/scaled dataframe
         """
+        df_pre_processed = df.copy()
+        
         if train == True :
             #continious var encoding :
             if len(continious_var) != 0 :
                 #continious var :
                 self.scaler = encoding_type_cont #StandardScaler() #or MinMaxScaler()
-                df[continious_var] = self.scaler.fit_transform(df[continious_var])    
+                df_pre_processed[continious_var] = self.scaler.fit_transform(df_pre_processed[continious_var])    
            
             #categorical encoding :
             if len(categorical_var_OHE) != 0 :
             #categorical var : OHE
-                self.enc_OHE = OneHotEncoder(drop='first', sparse=False).fit(df[categorical_var_OHE])
-                encoded = self.enc_OHE.transform(df[categorical_var_OHE])
+                self.enc_OHE = OneHotEncoder(drop='first', sparse=False).fit(df_pre_processed[categorical_var_OHE])
+                encoded = self.enc_OHE.transform(df_pre_processed[categorical_var_OHE])
                 encoded_df = pd.DataFrame(encoded,columns=self.enc_OHE.get_feature_names_out())
-                df.drop(categorical_var_OHE, axis=1, inplace=True)
-                df = pd.concat([df, encoded_df], axis=1)
+                df_pre_processed.drop(categorical_var_OHE, axis=1, inplace=True)
+                df_pre_processed = pd.concat([df_pre_processed, encoded_df], axis=1)
                
             if len(categorical_var_OrdinalEncoding) != 0 :
             #categorical var : Ordinal input example -> {"var" : {'c':0,'b':1,'a':2}}
                 for i in range(len(categorical_var_OrdinalEncoding)) :
                     var = list(categorical_var_OrdinalEncoding.keys())[i]
                     self.enc_ordinal = ce.OrdinalEncoder(cols=[var], return_df=True, mapping=[{'col':var,'mapping':categorical_var_OrdinalEncoding[var]}])
-                    df[var] = self.enc_ordinal.fit_transform(df[var])
+                    df_pre_processed[var] = self.enc_ordinal.fit_transform(df_pre_processed[var])
             
             if len(categorical_var_TE) != 0 :
             #categorical var : Target encoding
-                self.enc_TE = TargetEncoder().fit(df[categorical_var_TE], target)
-                encoded_TE = self.enc_TE.transform(df[categorical_var_TE])
+                self.enc_TE = TargetEncoder().fit(df_pre_processed[categorical_var_TE], target)
+                encoded_TE = self.enc_TE.transform(df_pre_processed[categorical_var_TE])
                 encoded_df_TE = pd.DataFrame(encoded_TE,columns=self.enc_TE.get_feature_names_out())
-                df.drop(categorical_var_TE, axis=1, inplace=True)
-                df = pd.concat([df, encoded_df_TE], axis=1)
+                df_pre_processed.drop(categorical_var_TE, axis=1, inplace=True)
+                df_pre_processed = pd.concat([df_pre_processed, encoded_df_TE], axis=1)
             
        
         else :
             #continious encoding :
             if len(continious_var) != 0 :
                 #continious var :
-                df[continious_var] = self.scaler.transform(df[continious_var])
+                df_pre_processed[continious_var] = self.scaler.transform(df_pre_processed[continious_var])
              
             #categorical encoding :  
             if len(categorical_var_OHE) != 0 :
                 #categorical var : OHE
-                encoded2 = self.enc_OHE.transform(df[categorical_var_OHE])
+                encoded2 = self.enc_OHE.transform(df_pre_processed[categorical_var_OHE])
                 encoded_df2 = pd.DataFrame(encoded2,columns=self.enc_OHE.get_feature_names_out())
-                df.drop(categorical_var_OHE, axis=1, inplace=True)
-                df = pd.concat([df, encoded_df2], axis=1)
+                df_pre_processed.drop(categorical_var_OHE, axis=1, inplace=True)
+                df_pre_processed = pd.concat([df_pre_processed, encoded_df2], axis=1)
            
             if len(categorical_var_OrdinalEncoding) != 0 :
             #categorical var : Ordinal input example -> {"var" : {'c':0,'b':1,'a':2}}
                 for i in range(len(categorical_var_OrdinalEncoding)) :
                     var = list(categorical_var_OrdinalEncoding.keys())[i]    
-                    df[var] = self.enc_ordinal.transform(df[var])
+                    df_pre_processed[var] = self.enc_ordinal.transform(df_pre_processed[var])
              
             if len(categorical_var_TE) != 0 :
                 #categorical var : Target Encoding
-                encoded2_TE = self.enc_TE.transform(df[categorical_var_TE])
+                encoded2_TE = self.enc_TE.transform(df_pre_processed[categorical_var_TE])
                 encoded_df2_TE = pd.DataFrame(encoded2_TE,columns=self.enc_TE.get_feature_names_out())
-                df.drop(categorical_var_TE, axis=1, inplace=True)
-                df = pd.concat([df, encoded_df2_TE], axis=1)
+                df_pre_processed.drop(categorical_var_TE, axis=1, inplace=True)
+                df_pre_processed = pd.concat([df_pre_processed, encoded_df2_TE], axis=1)
             
-        return df
+        return df_pre_processed
